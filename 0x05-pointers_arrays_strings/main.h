316fac123b7e8eedbe54e96c2525416bbf0de47f
tinetech1 <inegbuvalentine2020@gmail.com>
2023-10-16 19:42:16 -0700

    Write a function that takes a pointer to an int as parameter and updates the value it points to to 98

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <unistd.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -16,3 +17,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
+#endif
