dbcf2ec3edd89075233ab8d4ce89f77f8dbbf463
tinetech1 <inegbuvalentine2020@gmail.com>
2023-11-07 21:30:30 -0800

    new dog name

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,20 @@
  * @age: age of dog
  * @owner: name of owner
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+typedef struct dog dog_t;
+
 #endif
